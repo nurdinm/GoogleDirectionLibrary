7c76f87afcf42c5d4bdec1482b4bb0d375adf80d
DHD Mitra Indotama <dhdtekno@MacBook-Air-DHD.local>
2021-01-18 12:28:21 +0700

    change url

--- googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
+++ googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
@@ -22,7 +22,7 @@ package com.akexorcist.googledirection.constant;
  * For internal use.
  */
 public class DirectionUrl {
-    public static final String MAPS_API_URL = "https://maps.googleapis.com/maps/api/";
+    public static final String MAPS_API_URL = "https://apimart.pempekdehade.com/konsumen/v1/";
     public static final String DIRECTION_API_URL = "directions/json";
 
 }
