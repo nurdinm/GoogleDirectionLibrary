8f4bacb66bae26a68e7d1b57ff737954bc4c1480
nurdinm <muhammadnurdin460@gmail.com>
2021-01-27 09:39:52 +0700

    whattt

--- googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
+++ googleDirectionLibrary/src/main/java/com/akexorcist/googledirection/constant/DirectionUrl.java
@@ -22,7 +22,7 @@ package com.akexorcist.googledirection.constant;
  * For internal use.
  */
 public class DirectionUrl {
-    public static final String MAPS_API_URL = "https://apimart.pempekdehade.com/konsumen/v1/";
-    public static final String DIRECTION_API_URL = "directions/json";
+    public static final String MAPS_API_URL = "https://appint.ampunbangbajol.com/konsumen/v1/";
+    public static final String DIRECTION_API_URL = "nakkemano/apobae";
 
 }
